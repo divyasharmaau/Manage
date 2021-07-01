93edbfb0b4404dbff6ff40c679c4806e197e4cea
life T <lifetraveler128@gmail.com>
2021-07-02 09:06:20 +1000

    ErrorController, ILogger , nlog added, Startup File modified

---
+++ Manage.Web/Controllers/ErrorController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manage.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+        //incorrect url
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult =
+                    HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage =
+                            "Sorry, the resource you requested could not be found";
+
+                    _logger.LogWarning($"404 Error occured. Path ={statusCodeResult.OriginalPath}" +
+                        $"and QueryString = {statusCodeResult.OriginalQueryString}");
+                    //ViewBag.Path = statusCodeResult.OriginalPath;
+                    //ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    break;
+            }
+
+            return View("NotFound");
+        }
+
+
+        //unhandeled exception
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            _logger.LogError($"The path {exceptionDetails.Path} threw an exception" +
+                $" {exceptionDetails.Error}");
+            //ViewBag.ExceptionPath = exceptionDetails.Path;
+            //ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            //ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
+
+            return View("Error");
+
+        }
+
+    }
+}
