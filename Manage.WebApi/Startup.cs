f4078480a42812ebd42c44bdc7bcf06032680a21
ds <divyasharma.au@gmail.com>
2022-01-30 22:42:16 +1100

    Add Swagger and Swagger UI

--- Manage.WebApi/Startup.cs
+++ Manage.WebApi/Startup.cs
@@ -30,6 +30,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
 
 namespace Manage.WebApi
 {
@@ -149,6 +151,11 @@ namespace Manage.WebApi
                     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                 }
             );
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -169,10 +176,22 @@ namespace Manage.WebApi
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapSwagger();
             });
+
+            
+
+
         }
     }
 }
